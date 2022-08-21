e95be9f749ead005ac54ca4108280024ac8887b9
mbugua-richards <ricmwathih81@gmail.com>
2022-08-21 06:35:20 +0300

    tiktik_app

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import NoResults from '../components/NoResults';
+import VideoCard from '../components/VideoCard';
+import { Video } from '../types';
+
+interface IProps {
+     videos: Video[];
+ }
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      { videos.length ? (
+         videos.map((video: Video) => (
+          <VideoCard post={video} key={video.id}/>
+         ))
+         ) : (
+         <NoResults text={'No Videos'}/>
+      )}
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get('http://localhost:3000/api/post');  
+  return {
+    props: {
+      videos: data
+    }
+  }
+} 
+
+export default Home
